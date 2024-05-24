bdf2c7f316fc3c44d2d94818637e9246f1a46acd
nightSanches <galesavich@gmail.com>
2024-05-24 19:48:48 +0500

    Добавьте файлы проекта.

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pr37savichev.Data.Interfaces;
+
+namespace pr37savichev.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private IItems IAllItems;
+        private ICategories IAllCategories;
+
+
+        public ItemsController(IItems IAllItems, ICategories IAllCategories)
+        {
+            this.IAllItems = IAllItems;
+            this.IAllCategories = IAllCategories;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с предметами";
+            var cars = IAllItems.AllItems;
+            return View(cars);
+        }
+    }
+}
